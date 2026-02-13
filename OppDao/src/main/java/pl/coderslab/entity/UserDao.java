46b342f3acb672f0f32f930bd8633d8434b1a59d
smiernik1 <smiernik@visa.com>
2026-02-13 12:29:11 +0100

    Dodanie zapytań i funkcji create

--- OppDao/src/main/java/pl/coderslab/entity/UserDao.java
+++ OppDao/src/main/java/pl/coderslab/entity/UserDao.java
@@ -1,4 +1,50 @@
 package pl.coderslab.entity;
 
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.TaskMenager.DbUtil;
+
+import java.sql.*;
+
 public class UserDao {
+
+
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    private static final String READ_USER_QUERY =
+            "SELECT * FROM users WHERE id = ?";
+
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
+
+    private static final String DELETE_USER_QUERY =
+            "DELETE FROM users WHERE id = ?";
+
+    private static final String FIND_ALL_USERS_QUERY =
+            "SELECT * FROM users";
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
