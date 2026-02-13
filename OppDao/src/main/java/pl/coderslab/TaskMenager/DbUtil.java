7f2fd1f486e2690638935f2a2fdb1765c3346dca
smiernik1 <smiernik@visa.com>
2026-02-13 12:09:42 +0100

    Struktura projektu

---
+++ OppDao/src/main/java/pl/coderslab/TaskMenager/DbUtil.java
@@ -0,0 +1,4 @@
+package pl.coderslab.TaskMenager;
+
+public class DbUtil {
+}
---
+++ OppDao/src/main/java/pl/coderslab/TaskMenager/Main.java
@@ -0,0 +1,7 @@
+package pl.coderslab.TaskMenager;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
