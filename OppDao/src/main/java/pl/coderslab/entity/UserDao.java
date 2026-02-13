69c3022a29dd7a5ec516e3f568ee38dd6519c2d1
smiernik1 <smiernik@visa.com>
2026-02-13 12:30:00 +0100

    Dodanie metody read

--- OppDao/src/main/java/pl/coderslab/entity/UserDao.java
+++ OppDao/src/main/java/pl/coderslab/entity/UserDao.java
@@ -47,4 +47,28 @@ public class UserDao {
         }
     }
 
+    public User read(int userId) {
+
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
+
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+            return null;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
